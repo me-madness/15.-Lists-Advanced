91ca441f07fc994716b9fa246e25f5f12762c1cb
me_madness <p_rangel@abv.bg>
2023-10-11 18:59:44 +0200

    02.

---
+++ 02. trains.py
@@ -0,0 +1,20 @@
+wagons = [0] * int(input())
+
+while True:
+    command = input().split()
+    
+    if command == 'End':
+        print(wagons)
+        break
+    
+    elif command == 'add':
+        number_of_people = int(command[1])
+        wagons[-1] += number_of_people
+        
+    elif command[0] == 'insert':
+        index = int(command[1])    
+        people = int(command[2])    
+        
+    elif command[0] == 'insert':
+        index = int(command[1])    
+        people = int(command[1])     
