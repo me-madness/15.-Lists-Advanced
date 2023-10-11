b370d834d7044bf402b0ecff6eb0f48e87ad06f9
me_madness <p_rangel@abv.bg>
2023-10-11 19:10:53 +0200

    03. done

--- 03. to_do_list.py
+++ 03. to_do_list.py
@@ -10,7 +10,7 @@ def process_todo_notes():
         todo_notes.append(note)
         
     sorted_notes = sorted(todo_notes, key=lambda x: int(x.split('-')[0]))
-    return sorted_notes
+    return [note.split('-')[1] for note in sorted_notes]
 
 result = process_todo_notes()
 print(result)
