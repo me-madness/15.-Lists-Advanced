abb5e29dc564fb69f3819b1af28285b9e4d4ed2c
me_madness <p_rangel@abv.bg>
2023-10-11 19:44:02 +0200

    05. two options for the task are done

--- 05. sorting_names.py
+++ 05. sorting_names.py
@@ -1,5 +1,17 @@
 # First way from the Lector
 
+def sort_names():
+    names_list = [name for name in input().split(', ')]
+    
+    return sorted(names_list, key=lambda x: (-len(x), x))
 
+print(sort_names())
 
-# Second way from me 
+# Second way from me and lector 
+
+names = input().split(', ')
+sorted_names = sorted(names, key=len, reverse=True)
+new_sorted_names = sorted(names, key=lambda x: (-len(x), x))
+print(new_sorted_names)
+
+# Third way from me 
