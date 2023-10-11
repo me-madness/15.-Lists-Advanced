917a5638abbb59c79d23805c6d263b43b1e890e2
me_madness <p_rangel@abv.bg>
2023-10-11 19:08:23 +0200

    03.

--- 02. trains.py
+++ 02. trains.py
@@ -15,6 +15,7 @@ while True:
         index = int(command[1])    
         people = int(command[2])    
         
-    elif command[0] == 'insert':
+    elif command[0] == 'leave':
         index = int(command[1])    
-        people = int(command[1])     
+        people = int(command[2]) 
+            
