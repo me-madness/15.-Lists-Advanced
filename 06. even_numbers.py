5076c38ba9dbda48dd16a1416d1610bf01024fd1
me_madness <p_rangel@abv.bg>
2023-10-11 20:17:31 +0200

    07. done from the lector

--- 06. even_numbers.py
+++ 06. even_numbers.py
@@ -1,11 +1,13 @@
 # First way from the Lector
 
+   
+
+# Second way from me 
+
 def even_numbers():
-    numbers = input()
+    numbers = input().split(', ')
     
     even_number = list(filter(lambda x: x % 2 == 0), numbers)
     return even_number
 
-print(even_numbers())    
-
-# Second way from me 
+print(even_numbers())  
--- 07. the_office.py
+++ 07. the_office.py
@@ -1,4 +1,19 @@
 # First way from the Lector
 
+def office_happiness():
+    happiness_list = list(map(int, input().split()))
+    happiness_factor = int(input())
+                           
+    improved_happiness = [happiness * happiness_factor for happiness in happiness_list]
+    average_happiness = sum(improved_happiness) / len(improved_happiness)
+    happy_count = sum(happiness >= average_happiness for happiness in improved_happiness)
+    total_count = len(improved_happiness)
+    
+    message = 'happy' if happy_count >= total_count / 2 else 'not happy'
+    result = f'Score: {happy_count}/{total_count}. Employees {message}'
+    
+    return result
+
+print(office_happiness())
 
 # Second way from me 
